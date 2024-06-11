fd52939ae1de2f7edbdd7459e2becd85e5b203d3
TarGonzalez <tarjeterogonzalez@gmail.com>
2024-06-11 17:55:01 -0600

    [Utils] Se agrega params para tipos mascara fecha

--- lib/utils/fecha_utils.dart
+++ lib/utils/fecha_utils.dart
@@ -2,12 +2,26 @@ import 'package:intl/intl.dart';
 
 class FechaUtils {
   static String getFormatoFecha(
-      {required DateTime fecha, bool conHora = true}) {
+      {required DateTime fecha, bool conHora = true, int tipo = 1}) {
+    String fechaMask;
     String fechaFormat;
+    switch (tipo) {
+      // 01/01
+      case 2:
+        fechaMask = 'MM/dd';
+        break;
+      // Ene/01
+      case 3:
+        fechaMask = 'MMM/dd';
+        break;
+      // 01/01/0000
+      default:
+        fechaMask = 'dd/MM/yyyy';
+    }
     if (conHora) {
-      fechaFormat = DateFormat('dd/MM/yyyy hh:mm').format(fecha);
+      fechaFormat = DateFormat('$fechaMask hh:mm').format(fecha);
     } else {
-      fechaFormat = DateFormat('dd/MM/yyyy').format(fecha);
+      fechaFormat = DateFormat(fechaMask).format(fecha);
     }
     return fechaFormat;
   }
