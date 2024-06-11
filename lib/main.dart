ad81ff53094721cda0fb94d7472594800657c892
TarGonzalez <tarjeterogonzalez@gmail.com>
2024-06-11 17:56:17 -0600

    [Main] Se agrega default lg mex y orientacion portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 import '/routes/routes_names.dart';
 import '/routes/routes_pages.dart';
 import '/themes/app_theme.dart';
 import 'controllers/main_controller.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await initializeDateFormatting();
+  Intl.defaultLocale = 'es_MX';
   runApp(const MainApp());
 }
 
