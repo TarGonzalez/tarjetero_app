6a80f0ca7992a93d6eaa047b0b8502631d7aaf6e
miguel-GF <51388384+miguel-GF@users.noreply.github.com>
2024-06-12 17:01:53 -0600

    [Tarjetas] Se muestran campos de fechas en su widget

--- lib/widgets/tarjetas/tarjeta_widget.dart
+++ lib/widgets/tarjetas/tarjeta_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flip_card/flutter_flip_card.dart';
 import '../../helpers/tarjeta_helper.dart';
 import '../../models/tarjeta.dart';
+import '../../utils/fecha_utils.dart';
 
 class TarjetaWidget extends StatelessWidget {
   const TarjetaWidget({
@@ -67,39 +68,72 @@ class TarjetaWidget extends StatelessWidget {
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  const Spacer(),
-                  Text(
-                    tarjetaActual.titular != null
-                        ? tarjetaActual.titular!.toUpperCase()
-                        : '',
-                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                          color: Colors.white,
-                        ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const Spacer(),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  const Spacer(flex: 2),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        'Válida hasta',
-                        style:
-                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Fecha de corte',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .copyWith(
                                   color: Colors.white,
                                   fontSize: 16,
                                 ),
+                          ),
+                          Text(
+                            tarjetaActual.fechaCorte != null
+                                ? FechaUtils.getFormatoFecha(
+                                    fecha: FechaUtils.parseFecha(
+                                        fechaStr: tarjetaActual.fechaCorte!),
+                                    conHora: false,
+                                    tipo: 3,
+                                  )
+                                : '',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        tarjetaActual.expiracion != null
-                            ? tarjetaActual.expiracion!
-                            : '',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(
-                              color: Colors.white,
-                            ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Fecha de pago',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .copyWith(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                          ),
+                          Text(
+                            tarjetaActual.fechaPago != null
+                                ? FechaUtils.getFormatoFecha(
+                                    fecha: FechaUtils.parseFecha(
+                                        fechaStr: tarjetaActual.fechaPago!),
+                                    conHora: false,
+                                    tipo: 3,
+                                  )
+                                : '',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -120,27 +154,73 @@ class TarjetaWidget extends StatelessWidget {
                     color: Colors.white,
                     width: double.infinity,
                     height: 30,
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Text(
+                      tarjetaActual.titular != null
+                          ? tarjetaActual.titular!.toUpperCase()
+                          : '',
+                      style:
+                          Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                color: Colors.black87,
+                              ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
                   const SizedBox(height: 20),
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        'CVV',
-                        style:
-                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Válido hasta',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .copyWith(
                                   color: Colors.white,
                                 ),
+                          ),
+                          Text(
+                            tarjetaActual.expiracion != null
+                                ? tarjetaActual.expiracion!
+                                : '',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        tarjetaActual.codigo != null
-                            ? tarjetaActual.codigo!
-                            : '',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(
-                              color: Colors.white,
-                            ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'CVV',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                          ),
+                          Text(
+                            tarjetaActual.codigo != null
+                                ? tarjetaActual.codigo!
+                                : '',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
