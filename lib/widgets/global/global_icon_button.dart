b449224b02e620d07ce3d9afd13f267598764e3a
Miguel <miguel.gf.130@gmail.com>
2024-06-26 22:20:14 -0600

    [Widgets] Se agrega un icon button custom

---
+++ lib/widgets/global/global_icon_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class GlobalIconButton extends StatelessWidget {
+  const GlobalIconButton({
+    super.key,
+    this.icono,
+    this.onPressed,
+  });
+  final IconData? icono;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onPressed!();
+      },
+      borderRadius: BorderRadius.circular(
+        42,
+      ), // Esto es importante para que el InkWell respete el radio
+      child: Material(
+        color: Colors.transparent,
+        child: Ink(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(42),
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            width: 48,
+            height: 48,
+            child: Icon(
+              icono,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
