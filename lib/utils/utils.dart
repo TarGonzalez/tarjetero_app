3b0bf180c3dc3be84139aa53cd0ede1a026ddd9c
Miguel <miguel.gf.130@gmail.com>
2024-07-05 20:19:28 -0600

    [Utils] Se agrega el método para cambiar theme y pueda ser utilizado desde varios lados

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,11 @@
 import 'dart:io';
 
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '/constants.dart';
+import '/themes/app_theme.dart';
+
 class Utils {
   static Future<bool> validarConexion() async {
     try {
@@ -14,5 +20,24 @@ class Utils {
 
   static String limpiarException(Object e) {
     return e.toString().replaceAll('Exception: ', '');
+  }
+
+  static Future<void> cambiarTheme({required String themeMode}) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (themeMode == 'default') {
+      // final Brightness brightness =
+      //     WidgetsBinding.instance.platformDispatcher.platformBrightness;
+      // themeMode = brightness == Brightness.dark ? 'oscuro' : 'light';
+      final int hour = DateTime.now().hour;
+      themeMode = hour >= 19 || hour < 7 ? 'oscuro' : 'light';
+      await prefs.setString('theme', themeTipoDefault);
+    }
+    if (themeMode == 'light') {
+      Get.changeTheme(themeLight);
+      await prefs.setString('theme', themeTipoLight);
+    } else {
+      Get.changeTheme(themeDark);
+      await prefs.setString('theme', themeTipoDark);
+    }
   }
 }
