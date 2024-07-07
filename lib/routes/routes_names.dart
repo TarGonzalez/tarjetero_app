eb788e0dbf4a089d8c886882f9772c835c6011dc
Miguel <miguel.gf.130@gmail.com>
2024-07-06 20:15:01 -0600

    [Routes] Se agrega nueva ruta de registro nuevo cliente

--- lib/routes/routes_names.dart
+++ lib/routes/routes_names.dart
@@ -1,4 +1,5 @@
 const String nameLoginScreen = '/login';
+const String nameRegisterScreen = '/register';
 const String nameOnboardingScreen = '/onboarding';
 const String nameTabsScreen = '/tabs';
 const String nameNuevaTarjetaScreen = '/tarjetas/alta';
--- lib/routes/routes_pages.dart
+++ lib/routes/routes_pages.dart
@@ -4,6 +4,7 @@ import '/routes/routes_names.dart';
 import '/screens/gastos/nuevo_gasto_screen.dart';
 import '/screens/login_screen.dart';
 import '/screens/onboarding_screen.dart';
+import '/screens/register_screen.dart';
 import '/screens/tabs_screen.dart';
 import '/screens/tarjetas/nueva_tarjeta_screen.dart';
 
@@ -11,6 +12,7 @@ List<GetPage<dynamic>> rutas = <GetPage<dynamic>>[
   GetPage<dynamic>(
       name: nameOnboardingScreen, page: () => const OnboardingScreen()),
   GetPage<dynamic>(name: nameLoginScreen, page: () => const LoginScreen()),
+  GetPage<dynamic>(name: nameRegisterScreen, page: () => const RegisterScreen()),
   GetPage<dynamic>(name: nameTabsScreen, page: () => const TabsScreen()),
   GetPage<dynamic>(name: nameNuevaTarjetaScreen, page: () => const NuevaTarjetaScreen()),
   GetPage<dynamic>(name: nameNuevoGastoScreen, page: () => const NuevoGastoScreen()),
