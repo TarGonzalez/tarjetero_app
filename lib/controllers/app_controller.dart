6d2b65c204e2c82b2d6ec1c40d419191136c2880
Miguel <miguel.gf.130@gmail.com>
2024-07-13 19:23:24 -0600

    [App] Se agrega variable global de tarjeta, tipoTarjeta y método para limpiar dichas variables en controller

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,8 +1,16 @@
 import 'package:get/get.dart';
 
 import '../models/sesion.dart';
+import '../models/tarjeta.dart';
 
 class AppController extends GetxController {
   Rx<Sesion> sesionObj = Rx<Sesion>(Sesion());
   Rx<String> theme = Rx<String>('');
+  Rx<Tarjeta> tarjeta = Tarjeta().obs;
+  Rx<bool> tipoTarjeta = Rx<bool>(false);
+
+  Future<void> limpiarTarjetaActual() async {
+    tarjeta.value = Tarjeta();
+    tipoTarjeta.value = false;
+  }
 }
