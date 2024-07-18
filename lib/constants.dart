f3905d731390b6162e8d5e8db5ec3948d98df7f6
Miguel <miguel.gf.130@gmail.com>
2024-07-17 19:59:31 -0600

    [Cosntantes] Se agrega nuevas constantes para tarjetas tipos

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,9 @@ const String themeTipoDefault = 'default';
 const String themeTipoLight = 'light';
 const String themeTipoDark = 'dark';
 
+const int tarjetaTipoTarjetero = 1;
+const int tarjetaTipoGastos = 2;
+
 // COLORES A SELECCIONAR
 const List<String> coloresSeleccion = <String>[
   'rojo',
