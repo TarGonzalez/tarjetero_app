fa8d6b814c548226124454a72cd31dfce27661a1
Miguel <miguel.gf.130@gmail.com>
2024-07-22 22:58:22 -0600

    [Tarjeta] En su helper se agregan métodos para mostrar/ocultar ciertos datos

--- lib/helpers/tarjeta_helper.dart
+++ lib/helpers/tarjeta_helper.dart
@@ -1,9 +1,43 @@
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
 class TarjetaHelper {
-  static String ponerMascaraNumero({required String numero}) {
-    final MaskedTextController mask =
+  static String ponerMascaraNumero(
+      {required String numero, bool ocultar = false}) {
+    MaskedTextController mask =
         MaskedTextController(text: numero, mask: '0000   0000   0000   0000');
+    if (ocultar) {
+      mask =
+          MaskedTextController(text: numero, mask: '****     ****     ****     0000');
+    }
     return mask.text;
+  }
+
+  static String fechaExpMostrar(
+      {required String anio,
+      required String mes,
+      bool ocultarInformacion = false}) {
+    return !ocultarInformacion ? '$mes/$anio' : '**/**';
+  }
+
+  static String codigoMostrar(
+      {required String codigoCvv, bool ocultarInformacion = false}) {
+    return !ocultarInformacion ? codigoCvv : '***';
+  }
+
+  static String numeroMostrar(
+      {required String numero,
+      required String ultimosDigitos,
+      bool ocultarInformacion = false}) {
+    if (!ocultarInformacion) {
+      return ponerMascaraNumero(
+        numero: numero,
+        ocultar: ocultarInformacion,
+      );
+    } else {
+      return ponerMascaraNumero(
+        numero: '************$ultimosDigitos',
+        ocultar: ocultarInformacion,
+      );
+    }
   }
 }
