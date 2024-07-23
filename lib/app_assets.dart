0f53660d12397d3ed3d0d5df30b871ed445689b1
Miguel <miguel.gf.130@gmail.com>
2024-07-22 22:57:30 -0600

    [Assets] Se agrega nuevo icono chip

--- lib/app_assets.dart
+++ lib/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
   static String jsonCreditCards = 'assets/animations/credit_cards.json';
   static String imgErrorUno = 'assets/images/error1.png';
+  static String iconChip = 'assets/icons/chip.png';
 }
